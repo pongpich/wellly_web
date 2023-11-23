b8cde5ea28afd9b0583dbd2798243da35e33e02a
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-23 23:07:55 +0700

    เพิ่มความสุง

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -48,7 +48,7 @@ const Detail = () => {
         >
           รางวัล
         </div>
-        <p>
+        <p className={style["message-content"]}>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
