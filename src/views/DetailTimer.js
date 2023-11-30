8c60fc7e503345693570464904212b4aef51d27c
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-30 18:00:45 +0700

    จับเวลา

--- src/views/DetailTimer.js
+++ src/views/DetailTimer.js
@@ -3,27 +3,63 @@ import NavbarWeb from "./layouts/NavbarWeb";
 import { useNavigate } from "react-router-dom";
 import style from "../assets/css/detail.module.css";
 import stop from "../assets/image/icon/stop.png";
+import Contextual from "../assets/image/icon/Contextual.png";
 
 const DetailTimer = () => {
   const navigate = useNavigate();
-  const [statusPlay, setStatusPlay] = useState(0);
   const [statusAbandon, setStatusAbandon] = useState(0);
+  const [totalSeconds, setTotalSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
 
-  const clickPlay = (event) => {
-    if (event == "play") {
-      setStatusPlay(1);
-    } else {
-      setStatusPlay(0);
-    }
-  };
   const clickFinish = () => {
     navigate("/detailRegister");
   };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTotalSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const formatTime = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedTime = `${padZero(hours)}:${padZero(minutes)}:${padZero(
+      remainingSeconds
+    )}`;
+    return formattedTime;
+  };
+
+  const padZero = (value) => {
+    return value < 10 ? `0${value}` : value;
+  };
+
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handlePause = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setTotalSeconds(0);
+  };
+
   return (
     <div>
       <div className={style["box-time"]}>
         <p className={style["time"]}>เวลา</p>
-        <p className={style["count-time"]}>00:12:45</p>
+        <p className={style["count-time"]}>{formatTime(totalSeconds)}</p>
         <div className={style["justify-around"]}>
           <div>
             <p className={style["text-walk"]}>ก้าวเดิน (ก้าว)</p>
@@ -35,27 +71,25 @@ const DetailTimer = () => {
           </div>
         </div>
       </div>
+
       <div className={style["run-1"]}>
         <div className={style["box-run"]}>
           <p className={style["time"]}>
             กรุณาอย่าปิดหน้าเว็บตลอดการบันทึกกิจกรรม
           </p>
           <div>
-            {statusPlay == 0 ? (
+            {isRunning ? (
               <div className={style["box-start-run"]}>
                 <img
                   src={stop}
                   className={style["img-stop"]}
-                  onClick={() => clickPlay("play")}
+                  onClick={handlePause}
                 />
               </div>
             ) : (
               <>
                 <div className={style["box-start-run"]}>
-                  <div
-                    className={style["btn-play"]}
-                    onClick={() => clickPlay("stop")}
-                  >
+                  <div className={style["btn-play"]} onClick={handleStart}>
                     เล่นต่อ
                   </div>
                   <div
@@ -65,21 +99,18 @@ const DetailTimer = () => {
                     เสร็จ
                   </div>
                 </div>
-                <p className={style["text-abandon"]}>ละทิ้ง</p>
+                <a
+                  className={style["text-abandon"]}
+                  data-bs-toggle="modal"
+                  data-bs-target="#staticBackdrop"
+                >
+                  ละทิ้ง
+                </a>
               </>
             )}
           </div>
         </div>
       </div>
-
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop"
-      >
-        Launch static backdrop modal
-      </button>
 
       <div className="run-time">
         <div
@@ -93,29 +124,22 @@ const DetailTimer = () => {
         >
           <div className={`${style["modal-bottom"]} ${"modal-dialog"}`}>
             <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                  Modal title
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body">...</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Understood
-                </button>
+              <div className="modal-body">
+                <div className={style["contextual"]}>
+                  <img src={Contextual} className={style["img-contextual"]} />
+                </div>
+                <p className={style["confirm-abandon"]}>
+                  ยืนยันการละทิ้งกิจกรรม
+                </p>
+                <div className={style["btn-box-abandon"]}>
+                  <div
+                    className={style["btn-play-Abandon"]}
+                    data-bs-dismiss="modal"
+                  >
+                    กลับ
+                  </div>
+                  <div className={style["btn-finish-Abandon"]}>ละทิ้ง</div>
+                </div>
               </div>
             </div>
           </div>
