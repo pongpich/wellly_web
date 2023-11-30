ced090beabc4fdba7ae6dc68cbe4c831fb517f76
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-11-30 16:32:29 +0700

    ทำ model

--- src/views/DetailTimer.js
+++ src/views/DetailTimer.js
@@ -35,7 +35,7 @@ const DetailTimer = () => {
           </div>
         </div>
       </div>
-      <div className={style["run-2"]}>
+      <div className={style["run-1"]}>
         <div className={style["box-run"]}>
           <p className={style["time"]}>
             กรุณาอย่าปิดหน้าเว็บตลอดการบันทึกกิจกรรม
@@ -68,6 +68,56 @@ const DetailTimer = () => {
                 <p className={style["text-abandon"]}>ละทิ้ง</p>
               </>
             )}
+          </div>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#staticBackdrop"
+      >
+        Launch static backdrop modal
+      </button>
+
+      <div className="run-time">
+        <div
+          className="modal fade"
+          id="staticBackdrop"
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          tabindex="-1"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className={`${style["modal-bottom"]} ${"modal-dialog"}`}>
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                  Modal title
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">...</div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">
+                  Understood
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
