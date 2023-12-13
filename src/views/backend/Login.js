b7d16c763e2afea3ba17497829598ba8699afb32
AOF <pongpich.nakages@planforfit.com>
2023-12-13 15:18:03 +0700

    login

--- src/views/backend/Login.js
+++ src/views/backend/Login.js
@@ -1,76 +1,103 @@
 import React, { useState, useEffect } from "react";
 import style from "../../assets/css/login.module.css";
-
+import { Link } from "react-router-dom";
+import Logo from "../../assets/image/icon/Logo.png";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import InputAdornment from '@mui/material/InputAdornment';
+// import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 const Login = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  เกี่ยวกับเรา
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  ติดต่อเรา
-                </a>
-              </li>
-            </ul>
-            <div>
-              <select
-                className="form-select"
-                aria-label="Default select example"
-              >
-                <option selected>ภาษาไทย</option>
-                <option value="1">ภาษาอังกฤษ</option>
-              </select>
+
+      <div className={style["grid-container1"]}>
+        <div className={style["grid-item1"]}>
+          <img src={Logo} className={style["Logo"]} />
+        </div>
+        <div className={style["grid-item2"]}>
+          <div className={style["grid-container2"]}>
+            <div className={style["grid-item3"]}>
+
+            </div>
+            <div className={style["grid-item3"]}>
+              <Link to="/login">
+                <span className={style["content"]}>ติดต่อเรา</span>
+              </Link>
+            </div>
+            <div className={style["grid-item4"]}>
+              <Link to="/login">
+                <span className={style["content"]}>Eng</span>
+              </Link>
             </div>
           </div>
         </div>
-      </nav>
-      <p className={style["event-creator"]}>Event Creator Log in</p>
-      <div className={style["from-login"]}>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="อีเมล"
+
+
+      </div >
+
+
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+
+        <Typography component="h1" variant="h5">
+          <p className={style["event-creator"]}>Event Creator </p>
+        </Typography>
+        <Box component="form" noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            fullWidth
+            id="email"
+            label="อีเมล"
+            name="email"
+            autoComplete="email"
+            autoFocus
           />
-        </div>
-        <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            password
-          </label>
-          <input
+          <TextField
+            margin="normal"
+            fullWidth
+            name="password"
+            label="รหัสผ่านอย่างน้อย 8 ตัวอักษร"
             type="password"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="รหัสผ่านอย่างน้อย 8 หลัก"
+            id="password"
+            autoComplete="current-password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AttachMoneyIcon />
+                </InputAdornment>
+              ),
+            }}
           />
-        </div>
-        <button type="button" class={`${style["button-login"]}`}>
-          ล็อกอิน
-        </button>
-        <p className={style["forgot-password"]}>ลืมรหัสผ่าน?</p>
-      </div>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 1 , borderRadius: '25px'}}
+            style={{  height: 48 }}
+          >
+            <p className={style["login-submit"]}>เข้าสู่ระบบ </p>
+          </Button>
+          <Link to="/login">
+            <p className={style["forgot-password"]}>ลืมรหัสผ่าน ?</p>
+          </Link>
+        </Box>
+      </Box>
+
     </>
   );
 };
